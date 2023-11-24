a7d4034c8060d2f485daa3e57ab99ea0ea48c45f
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-24 18:47:44 +0600

    second commit

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ var student2={
 }
 
 document.write(student1.name);
+
+function student(name,age,cgpa,lang){
+this.name=name;
+this.age=age;
+this.cgpa=cgpa;
+this.lang=lang;
+}
+document.write("<br>");
+var student3=new student("Sakib",21,1.65,["Bangla","Chinese","Japanese"]);
+var student4=new student("Momin",25,2.75,["Bangla","English","Japanese"]);
+var student5=new student("Rakib",27,2.61,["Bangla","Chinese","Korean"]);
+
+document.write(student3.cgpa + "<br>");
+document.write(student4.lang + "<br>");
