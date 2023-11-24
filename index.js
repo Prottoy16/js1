76677564c6047cc6715e7aaf8d5ac6d0f70211d6
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-24 18:38:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var student1= {
+    name : "Prottoy",
+    age : 23,
+    cgpa : 2.56,
+    language : ["Bangla","English","Hindi"]
+}
+
+var student2={
+    name : "Mehedi",
+    age : 24,
+    chpa : 2.53,
+    language : ["Bangla","Portuguese","Urdu"]
+}
+
+document.write(student1.name);
